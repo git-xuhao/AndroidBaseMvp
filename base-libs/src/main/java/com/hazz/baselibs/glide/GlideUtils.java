01d9e8f2c1e6566e59b7a202a00e30cec303b46b
XuHao <504105930@qq.com>
2018-06-26 15:19:30 +0800

    add GlideUtils

--- base-libs/src/main/java/com/hazz/baselibs/glide/GlideUtils.java
+++ base-libs/src/main/java/com/hazz/baselibs/glide/GlideUtils.java
@@ -6,5 +6,105 @@ package com.hazz.baselibs.glide;
  * @desc 图片加载工具类
  */
 public class GlideUtils {
+  
+  /**
+     * 加载图片
+     *
+     * @param context  context
+     * @param iv       imageView
+     * @param url      图片地址
+     * @param emptyImg 默认展位图
+     */
+    public static void loadImage(Context context, ImageView iv, String url, int emptyImg) {
+        if (!TextUtils.isEmpty(url)) {
+            GlideApp.with(context)
+                    .load(url)
+                    .error(emptyImg)
+                    .placeholder(iv.getDrawable())
+                    .transition(new DrawableTransitionOptions().crossFade())
+                    .into(iv);
+        } else {
+            loadImage(context, iv, emptyImg, emptyImg);
+        }
+    }
+
+    /**
+     * 加载圆角图片
+     *
+     * @param context  context
+     * @param iv       imageView
+     * @param url      图片地址
+     * @param emptyImg 默认展位图
+     */
+    public static void loadRoundImage(Context context, ImageView iv, String url, int emptyImg) {
+        if (!TextUtils.isEmpty(url)) {
+            GlideApp.with(context)
+                    .load(url)
+                    .error(emptyImg)
+                    .placeholder(iv.getDrawable())
+                    .transition(new DrawableTransitionOptions().crossFade())
+                    .transform(new RoundedCorners(20)).into(iv);
+        } else {
+            loadRoundImage(context, iv, emptyImg, emptyImg);
+        }
+    }
+
+    /**
+     * 加载圆形图片
+     *
+     * @param context  context
+     * @param iv       imageView
+     * @param url      图片地址
+     * @param emptyImg 默认展位图
+     */
+    public static void loadCircleImage(Context context, ImageView iv, String url, int emptyImg) {
+        if (!TextUtils.isEmpty(url)) {
+            GlideApp.with(context)
+                    .load(url)
+                    .error(emptyImg)
+                    .placeholder(iv.getDrawable())
+                    .transition(new DrawableTransitionOptions().crossFade())
+                    .transform(new CircleCrop()).into(iv);
+        } else {
+            loadCircleImage(context, iv, emptyImg, emptyImg);
+        }
+    }
+
+    /**
+     * 加载drawable中的图片资源
+     *
+     * @param context  context
+     * @param iv       imageView
+     * @param resId    图片地址
+     * @param emptyImg 默认展位图
+     */
+    public static void loadImage(Context context, ImageView iv, int resId, int emptyImg) {
+        GlideApp.with(context).load(resId).placeholder(emptyImg).into(iv);
+    }
+
+    /**
+     * 加载drawable中的图片资源 圆角
+     *
+     * @param context  context
+     * @param iv       imageView
+     * @param resId    图片地址
+     * @param emptyImg 默认展位图
+     */
+    public static void loadRoundImage(Context context, ImageView iv, int resId, int emptyImg) {
+        GlideApp.with(context).load(emptyImg).placeholder(emptyImg).transform(new RoundedCorners(20)).into(iv);
+    }
+
+    /**
+     * 加载drawable中的图片资源 圆形
+     *
+     * @param context  context
+     * @param iv       imageView
+     * @param resId    图片地址
+     * @param emptyImg 默认展位图
+     */
+    public static void loadCircleImage(Context context, ImageView iv, int resId, int emptyImg) {
+        GlideApp.with(context).load(emptyImg).placeholder(emptyImg).transform(new CircleCrop()).into(iv);
+    }
+
 
 }
